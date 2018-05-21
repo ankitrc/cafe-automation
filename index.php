76549fb3faff2b413efed074ca15af38efd3613d
ankitrc <ankitrcpareek@gmail.com>
2018-05-21 07:57:00 +0530

    first commit test

--- index.php
+++ index.php
@@ -1,25 +1,3 @@
 <?php
-    include_once './app/view/html/static/header.html';
-    // $x = NULL;
-    if(isset($_POST['staff_login'])){
-        include_once './app/view/html/login/login.html';
-    }
-    else if(isset($_POST['menu'])){
-        include_once './app/view/html/login/menu.html';
-    }
-    else{
-        include_once './app/view/html/login/menu.html';
-    }
-
-    if(!isset($_POST['staff_login'])){
-        // <form action="">
-        // </form>
-        include_once './app/view/html/static/login_button.html';
-        // echo $_POST['staff_id'];
-    }
-    else{
-        include_once './app/view/html/static/menu_button.html';
-    }
-
-    include_once './app/view/html/static/footer.html'
+    echo 'cafe automation test';
 ?>
