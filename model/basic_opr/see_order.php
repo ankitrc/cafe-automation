0f107acd73fa6db2486ead7d6fab08313653d875
ankitrc <ankitrcpareek@gmail.com>
2018-05-21 10:49:42 +0530

    update staff operations

--- model/basic_opr/see_order.php
+++ model/basic_opr/see_order.php
@@ -10,7 +10,8 @@
         die('not autorized');
     }
 
-    $sql = "select * from cust_order where order_cid in(select cid from customer where status = 0)";
+    $sql = "select customer.table_no as table_no, cust_order.food_id as food_id, cust_order.food_name as food_name , 
+        cust_order.quantity as quantity from cust_order inner join customer on cust_order.order_cid = customer.cid   where customer.status = 0";
     if($result = $conn->query($sql)){
         // echo "ok";
     }
@@ -18,9 +19,9 @@
         echo "helllo";
         echo $conn->error;  
     }
-    echo 'tab_no'."   ".'food_id'."  ".'food_name'."   ".'quantity'."<br>";
+    echo 'table_no'."   ".'food_id'."  ".'food_name'."   ".'quantity'."<br>";
     // if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()){
-            echo $row['tab_no']."   ".$row['food_id']."  ".$row['food_name']."   ".$row['quantity']."<br>";
+            echo $row['table_no']."   ".$row['food_id']."  ".$row['food_name']."   ".$row['quantity']."<br>";
         }
 ?>
